ba8884a78630245045fcddfb0db967ff38a48ee9
Justin <jribarich98@gmail.com>
2022-07-20 20:12:53 -0400

    Mostly finished product
    - Can you believe I spent 8 hours on this today.
    - Cant wait to spend another 8 hours trying to figure out how to get it
    to work on mobile.

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const billValue = document.getElementById('bill');
+const selector = document.querySelector('.custom-select select');
+const btnSubmit = document.querySelector('.submit');
+const openModal = document.querySelector('.modal');
+const closeModal = document.querySelector('.close-modal');
+
+const setBill = document.querySelector('.user-amount');
+const setService = document.querySelector('.service-choice');
+const setTipPercent = document.querySelector('.tip-percent');
+const setTipAmount = document.querySelector('.tip-amount');
+
+btnSubmit.addEventListener('click', () => {
+  // Calculate tip value
+  const tipPercent = selector.value == 1 ? 20 : 15;
+  const userBill = billValue.value;
+  const tip = ((tipPercent / 100) * userBill).toFixed(2);
+  // Display modal
+  openModal.classList.remove('hidden');
+  setBill.textContent = userBill;
+  setService.textContent =
+    selector.value == 1 ? 'great service 👍' : 'bad service 👎';
+  setTipPercent.textContent = tipPercent;
+  setTipAmount.textContent = tip;
+});
+
+closeModal.addEventListener('click', () => {
+  openModal.classList.add('hidden');
+});
